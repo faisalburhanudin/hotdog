db35838998383686ee563f5bf306e97b06ae96db
faisalburhanudin <faisalburhanudin@gmail.com>
2018-02-11 22:09:33 +0700

    change model

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -p 8888:8888 -p 6006:6006 -v `pwd`:/root/workspace -m 4g dl bash
+docker run --rm -it -p 8888:8888 -p 6006:6006 -v `pwd`:/root/workspace -m 4g faisalburhanudin/dl bash
